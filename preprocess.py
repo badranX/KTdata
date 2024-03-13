2ec8d46419cd0cbf6c9ca84fe551926208d24750
Yahya Badran <techtweaking@gmail.com>
2024-03-13 01:08:13 +0100

    fix kc issue

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,20 @@
 import yamld
 
-n_dub = 2
+n_dub = 1
 
 meta = yamld.read_metadata('./assist2009.yaml')
 
 problem2KCs = meta['problem2KCs']
 n_kc = meta['n_kc']
+avg_kc_per_exer = meta['avg_kc_per_exer']
 
 def generate_correlated_kc(kcs):
-    kcs1 = map(lambda x: x + n_kc, kcs)
-    kcs2 = map(lambda x: x + 2*n_kc, kcs)
-    return kcs + [list(kcs1)]*(n_dub-1)
+    new = list(kcs)
+    for i in range(1, n_dub+1):
+        kcs1 = list(map(lambda x: x + i*n_kc, kcs))
+        new.extend(kcs1)
 
+    return new
 l = map(generate_correlated_kc, problem2KCs)
 
 l = list(l)
@@ -19,7 +22,8 @@ l = list(l)
 df = yamld.read_dataframe('./assist2009.yaml')
 
 df.attrs['problem2KCs'] = l
-df.attrs['n_kc'] = n_kc*n_dub
+df.attrs['n_kc'] = n_kc*(n_dub + 1)
+df.attrs['avg_kc_per_exer'] = avg_kc_per_exer*(n_dub + 1)
 
 yamld.write_dataframe('./corr_assist2009.yaml', df)
 
